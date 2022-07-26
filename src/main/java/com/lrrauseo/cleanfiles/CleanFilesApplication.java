b6f074618a7ae2da8addb1c38378dfa844a47ee1
Leandro Rauseo <leandro.rauseo@gmail.com>
2022-07-26 10:29:16 -0300

    inclusão da configuração de formato de data para o arquivo a ser excluído Criação de logs para o processo

--- src/main/java/com/lrrauseo/cleanfiles/CleanFilesApplication.java
+++ src/main/java/com/lrrauseo/cleanfiles/CleanFilesApplication.java
@@ -1,19 +1,21 @@
 package com.lrrauseo.cleanfiles;
 
 import java.io.File;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
+
+import lombok.extern.log4j.Log4j2;
 
 @SpringBootApplication
+@Log4j2
 public class CleanFilesApplication implements CommandLineRunner {
 
 	@Value("${application.base_path}")
@@ -21,6 +23,9 @@ public class CleanFilesApplication implements CommandLineRunner {
 
 	@Value("${application.expiration_days}")
 	private int expirationDays;
+
+	@Value("${application.date_format}")
+	private String dateFormat;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CleanFilesApplication.class, args);
@@ -33,20 +38,22 @@ public class CleanFilesApplication implements CommandLineRunner {
 
 		calendar.add(Calendar.DAY_OF_MONTH, -expirationDays);
 
-		// System.out.println("classpath: " +
-		// ClassPathResource.class.getResource("/").getPath());
-
-		System.out.println("Limit Date: " + new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime()));
+		log.info("Limit Date: " + new SimpleDateFormat(dateFormat).format(calendar.getTime()));
 		for (File fileEntry : baseDirectory.listFiles()) {
 			if (fileEntry.isDirectory()) {
-				System.out.println("Directory: " + fileEntry.getName());
-				Date arquivoDate = new SimpleDateFormat("yyyyMMdd").parse(fileEntry.getName());
-				if (arquivoDate.before(calendar.getTime())) {
-					System.out.println("Deleting: " + fileEntry.getName());
-					FileUtils.deleteDirectory(fileEntry);
+				log.info("Directory: " + fileEntry.getName());
+				try {
+
+					Date arquivoDate = new SimpleDateFormat(dateFormat).parse(fileEntry.getName());
+					if (arquivoDate.before(calendar.getTime())) {
+						log.info("Deleting: " + fileEntry.getName());
+						FileUtils.deleteDirectory(fileEntry);
+					}
+				} catch (ParseException e) {
+					log.warn("Não foi possivel excluir o arquivo '{}' nomenclatura desconhecida para exclusão", fileEntry.getName());
 				}
 			} else {
-				System.out.println("File: " + fileEntry.getName());
+				log.info("File: " + fileEntry.getName());
 			}
 		}
 
